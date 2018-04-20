e9199cd459546dcc910e2adf79071a0e0f045e1d
rakeemr <raqe-11@hotmail.com>
2018-04-20 05:55:09 -0600

    [ADD] Home page begining

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.parallax');
+var instance = M.Parallax.init(elem, options);
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
